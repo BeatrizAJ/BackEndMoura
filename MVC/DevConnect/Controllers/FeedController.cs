e3d9eeb50dab72af901dd225b8625782c77eaea4
Beatriz <biaajesus00@gmail.com>
2025-12-03 16:38:10 -0300

    3/12

---
+++ MVC/DevConnect/Controllers/FeedController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DevConnect.Controllers
+{
+    [Route("[controller]")]
+    public class Feed : Controller
+    {
+        private readonly ILogger<Feed> _logger;
+
+        public Feed(ILogger<Feed> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
